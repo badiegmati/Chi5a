9ddb40cf02217e23de8e63ab15b6fdc8e391af1e
badiegmati <badiegmati11@gmail.com>
2025-08-01 15:01:28 +0100

    Update vite.config.js
    2 update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  assetsInclude:['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
   base: '/Chi5a/',
   plugins: [react(),
     tailwindcss(),
